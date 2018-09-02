a68a5cd1e4d4c4e1cb773596f65303e396863797
tsu-kaneko <gamuuma@gmail.com>
2018-09-03 01:56:07 +0900

    編集機能の追加（〜Angular 6入門 02: 編集ページをつくる）

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroinesComponent } from './heroines/heroines.component';
@@ -10,7 +11,8 @@ import { HeroinesComponent } from './heroines/heroines.component';
     HeroinesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroines/heroines.component.ts
+++ src/app/heroines/heroines.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Heroine} from '../Heroine';
 
 @Component({
   selector: 'app-heroines',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroines.component.css']
 })
 export class HeroinesComponent implements OnInit {
+
+  heroine: Heroine = {
+    id: 1,
+    name: 'シータ'
+  }
 
   constructor() { }
 
