13d494a6fe85fb3d207d9f2fd5f6832a09573438
tsu-kaneko <gamuuma@gmail.com>
2018-09-03 14:14:22 +0900

    メッセージServiceにメッセージを追加する処理の追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroinesComponent } from './heroines/heroines.component';
 import { HeroineDetailComponent } from './heroine-detail/heroine-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroinesComponent,
-    HeroineDetailComponent
+    HeroineDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/heroine.service.ts
+++ src/app/heroine.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { Heroine } from './Heroine';
 import { HEROINES } from './mock-heroines';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroineService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroines(): Observable<Heroine[]> {
+    this.messageService.add('HeroineService: データを取得');
+    console.log(this.messageService);
     return of(HEROINES);
   }
 }
